1b9370f8b3e23c03b2d00690e97bbe0305d9d79f
azeek21 <pymanuz@gmail.com>
2023-05-25 22:08:08 +0500

    feat: add swiper

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -86,39 +86,119 @@
             <h2 class="section__title">
                 Наши продукты производится новейшими технологиями
             </h2>
-            <div class="all-products__item">
-                <div class="all-products__item-image">
-                    <img src="../assets/img/pp4.png" alt="">
-                </div>
-                <div class="all-products__item-content">
-                    <p class="section__text fw_700 mba">1/3</p>
-    
-                    <div>
-                        <h3 class="card__title">
-                            Schlafhorst Autoconer 6
-                        </h3>
-                        <p class="section__text">
-                            Встречайте новый Nissan Pathfinder! Оцените по достоинству его впечатляющий дизайн. Выразительные простые линии придают кузову современный, решительный внешний вид. Хотите проявить больше индивидуальности?
-                            
-                        </p>
-                        <p class="section__text">
-                            Выбирайте двухцветную окраску кузова!Светодиодные фары и ходовые огни в сочетании с массивной решеткой радиатора V–Motion делают внешность Nissan Pathfinder заметной и запоминающейся.
-                        </p>
-                    </div>
-    
-                    <div class="mta button__container">
-                        <span class="button">
-                            <img src="../assets/img/arrow-left_outlined.svg" alt="">
-                        </span>
-                        <span class="button">
-                            <img src="../assets/img/arrow-right_outlined.svg" alt="">
-                        </span>
-                    </div>
-    
-                </div>
+            <div class="width_100">
+                    <swiper
+                        :slides-per-view="1"
+                        :space-between="50"
+                        @swiper="onSwiper"
+                        @slideChange="onSlideChange"
+                        :ref="swiper"
+                        >
+                            <swiper-slide>
+                                <div class="all-products__item pad_ver_1">
+                                    <div class="all-products__item-image">
+                                        <img src="../assets/img/pp4.png" alt="">
+                                    </div>
+                                    <div class="all-products__item-content">
+                                        <p class="section__text fw_700 mba">1/3</p>
+                        
+                                        <div>
+                                            <h3 class="card__title">
+                                                Schlafhorst Autoconer 6
+                                            </h3>
+                                            <p class="section__text">
+                                                Встречайте новый Nissan Pathfinder! Оцените по достоинству его впечатляющий дизайн. Выразительные простые линии придают кузову современный, решительный внешний вид. Хотите проявить больше индивидуальности?
+                                                
+                                            </p>
+                                            <p class="section__text">
+                                                Выбирайте двухцветную окраску кузова!Светодиодные фары и ходовые огни в сочетании с массивной решеткой радиатора V–Motion делают внешность Nissan Pathfinder заметной и запоминающейся.
+                                            </p>
+                                        </div>
+                        
+                                        <div class="mta button__container">
+                                            <span class="button" @click="prevSlide()">
+                                                <img src="../assets/img/arrow-left_outlined.svg" alt="">
+                                            </span>
+                                            <span class="button" @click="nextSlide()">
+                                                <img src="../assets/img/arrow-right_outlined.svg" alt="">
+                                            </span>
+                                        </div>
+                        
+                                    </div>
+                                </div>
+                            </swiper-slide>
+
+                            <swiper-slide>
+                                <div class="all-products__item">
+                                    <div class="all-products__item-image">
+                                        <img src="../assets/img/pp4.png" alt="">
+                                    </div>
+                                    <div class="all-products__item-content">
+                                        <p class="section__text fw_700 mba">1/3</p>
+                        
+                                        <div>
+                                            <h3 class="card__title">
+                                                Schlafhorst Autoconer 6
+                                            </h3>
+                                            <p class="section__text">
+                                                Встречайте новый Nissan Pathfinder! Оцените по достоинству его впечатляющий дизайн. Выразительные простые линии придают кузову современный, решительный внешний вид. Хотите проявить больше индивидуальности?
+                                                
+                                            </p>
+                                            <p class="section__text">
+                                                Выбирайте двухцветную окраску кузова!Светодиодные фары и ходовые огни в сочетании с массивной решеткой радиатора V–Motion делают внешность Nissan Pathfinder заметной и запоминающейся.
+                                            </p>
+                                        </div>
+                        
+                                        <div class="mta button__container">
+                                            <span class="button" @click="prevSlide()">
+                                                <img src="../assets/img/arrow-left_outlined.svg" alt="">
+                                            </span>
+                                            <span class="button" @click="nextSlide()">
+                                                <img src="../assets/img/arrow-right_outlined.svg" alt="">
+                                            </span>
+                                        </div>
+                        
+                                    </div>
+                                </div>
+                            </swiper-slide>
+
+                            <swiper-slide>
+                                <div class="all-products__item">
+                                    <div class="all-products__item-image">
+                                        <img src="../assets/img/pp4.png" alt="">
+                                    </div>
+                                    <div class="all-products__item-content">
+                                        <p class="section__text fw_700 mba">1/3</p>
+                        
+                                        <div>
+                                            <h3 class="card__title">
+                                                Schlafhorst Autoconer 6
+                                            </h3>
+                                            <p class="section__text">
+                                                Встречайте новый Nissan Pathfinder! Оцените по достоинству его впечатляющий дизайн. Выразительные простые линии придают кузову современный, решительный внешний вид. Хотите проявить больше индивидуальности?
+                                                
+                                            </p>
+                                            <p class="section__text">
+                                                Выбирайте двухцветную окраску кузова!Светодиодные фары и ходовые огни в сочетании с массивной решеткой радиатора V–Motion делают внешность Nissan Pathfinder заметной и запоминающейся.
+                                            </p>
+                                        </div>
+                        
+                                        <div class="mta button__container">
+                                            <span class="button" @click="prevSlide()">
+                                                <img src="../assets/img/arrow-left_outlined.svg" alt="">
+                                            </span>
+                                            <span class="button" @click="nextSlide()">
+                                                <img src="../assets/img/arrow-right_outlined.svg" alt="">
+                                            </span>
+                                        </div>
+                        
+                                    </div>
+                                </div>
+                            </swiper-slide>
+                    </swiper>
+                
             </div>
         </div>
-    
     
         <div class="section section3 padding_normal">
             <div class="padding_normal bg_green_dark pad_ver_5">
@@ -138,10 +218,38 @@
 <script>
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
+import {Swiper, SwiperSlide} from 'swiper/vue';
+import { ref } from 'vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 export default {
     components: {
         Navbar,
-        Footer
+        Footer,
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        const nextSlide = ref(null);
+        const prevSlide = ref(null);
+        const onSwiper = (swiper) => {
+            console.log(swiper);
+            nextSlide.value = () => {swiper.slideNext()};   
+            prevSlide.value = () => {swiper.slidePrev()};
+            console.log(swiper.slideNext);
+        };
+        const onSlideChange = () => {
+            console.log('slide change');
+        };
+        const swiper = ref(null);
+        return {
+            onSwiper,
+            onSlideChange,
+            swiper,
+            nextSlide,
+            prevSlide,
+        };
     }
 }
 
